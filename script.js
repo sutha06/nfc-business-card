dd065ca4fc69a0698872cc3bd40c2c512e42366b
sutha06 <suthakaran.siva0601@gmail.com>
2025-10-21 13:04:21 -0400

    Version 3

--- script.js
+++ script.js
@@ -21,43 +21,25 @@ function closeModal() {
 }
 
 function copyPortfolioLink() {
+  const button = document.querySelector(".modal-btn");
+  const feedback = document.getElementById("copyFeedback");
+
+  // Store original content
+  const originalContent = button.innerHTML;
+
   navigator.clipboard.writeText(PORTFOLIO_URL).then(() => {
-    const feedback = document.getElementById("copyFeedback");
+    // Clear button content and add copied class
+    button.innerHTML = "";
+    button.classList.add("copied");
     feedback.classList.add("show");
+
+    // Reset after 2 seconds
     setTimeout(() => {
+      button.classList.remove("copied");
+      button.innerHTML = originalContent;
       feedback.classList.remove("show");
     }, 2000);
   });
-}
-
-function showLinks() {
-  const linksSection = document.getElementById("linksSection");
-  linksSection.classList.toggle("hidden");
-}
-
-function saveContact() {
-  // Create vCard content
-  const vCard = `BEGIN:VCARD
-VERSION:3.0
-FN:Suthakaran Siva
-N:Siva;Suthakaran;;;
-TITLE:IT Support & System Administration
-EMAIL;TYPE=INTERNET:suthakaran.siva0601@gmail.com
-TEL;TYPE=CELL:+14167107465
-URL:https://github.com/sutha06
-ADR;TYPE=HOME:;;Toronto;Ontario;;Canada
-END:VCARD`;
-
-  // Create blob and download
-  const blob = new Blob([vCard], { type: "text/vcard" });
-  const url = window.URL.createObjectURL(blob);
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "Suthakaran_Siva.vcf";
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  window.URL.revokeObjectURL(url);
 }
 
 // Close modal when clicking outside
